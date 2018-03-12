3a62cba338559f45c4e3136057b6642aaad8d23c
schmidtp0740 <schmidtp0740@gmail.com>
2018-03-12 10:27:17 -0500

    changed url

--- src/app/services/API/blockAPI.js
+++ src/app/services/API/blockAPI.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//const url = 'http://129.146.106.151:4001/bcsgw/rest/v1/transaction/query';
-const url = 'http://localhost:8000/getData'
+const url = 'http://129.146.106.151:4001/bcsgw/rest/v1/transaction/query';
 export async function getAllData(){
   console.log(url);  
   return axios.get(url)
